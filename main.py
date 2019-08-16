11d9719d60ba3aebe88246d5bfac87214a92e3f5
Dibyajitdj <dibyajittripathy97@gmail.com>
2019-08-16 16:43:27 +0530

    FIxed Some Issues of Selecting the Rcpt.

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
 from selenium import webdriver
+import time
 
 driver = webdriver.Firefox()
 driver.get("https://web.whatsapp.com/")
@@ -9,6 +10,12 @@ msg=input("Enter the message")
 no=int(input("Enter no of times"))
 
 input("Enter any thing after Scanning the code")
+
+sch=driver.find_element_by_class_name('_2zCfw')
+sch.send_keys(rcpt)
+
+press=driver.find_element_by_class_name('_2UaNq')
+press.click()
 
 elem = driver.find_element_by_xpath('//span[@title = "{}"]'.format(rcpt))
 elem.click()
